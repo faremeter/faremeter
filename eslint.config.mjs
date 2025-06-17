d063717939f480a1e9977a1fdc829359c32be4ac
Alexander Guy <alexander.guy@pm.me>
2025-06-17 12:21:40 -0700

    Add ignores to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,11 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
+  globalIgnores(["**/idl_type.ts", "**/dist/**"]),
 );

88dcc943996b3bf0ca3324c43deedac05e425fc6
Alexander Guy <alexander.guy@pm.me>
2025-07-11 15:45:45 -0700

    Bring in @FarinM's example scripts

---
+++ scripts/solana-example/server.ts
@@ -0,0 +1,36 @@
+import { default as express } from "express";
+import type { Request, Response } from "express";
+import { paymentMiddleware } from "@faremeter/x402-solana/middleware";
+import { Keypair } from "@solana/web3.js";
+import fs from "fs";
+
+const adminKeypair = Keypair.fromSecretKey(
+  Uint8Array.from(
+    JSON.parse(fs.readFileSync("../keypairs/admin.json", "utf-8")),
+  ),
+);
+
+const run = async () => {
+  const app = express();
+
+  app.get(
+    "/protected",
+    paymentMiddleware(
+      {
+        receiver: Keypair.generate().publicKey,
+        amount: 1000000,
+        admin: adminKeypair.publicKey,
+      },
+      adminKeypair,
+    ),
+    (req: Request, res: Response) => {
+      res.json({
+        msg: "success",
+      });
+    },
+  );
+
+  app.listen(3000);
+};
+
+run();

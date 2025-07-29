17864599b60b79dab96a2991e54d797a41723860
Alexander Guy <alexander.guy@pm.me>
2025-07-28 19:49:09 -0700

    Provide some pass-through middleware to talk to the facilitator

--- packages/middleware/src/express.ts
+++ packages/middleware/src/express.ts
@@ -1,5 +1,8 @@
 import {
   type FacilitatorHandler,
+  x402SettleRequest,
+  x402SettleResponse,
+  x402PaymentRequiredResponse,
   x402PaymentRequirements,
   x402PaymentHeaderToPayload,
   isValidationError,
@@ -73,5 +76,88 @@ export function createDirectFacilitatorMiddleware(
     }
 
     return sendPaymentRequired(res);
+  };
+}
+
+type CreateMiddlewareArgs = {
+  accepts: x402PaymentRequirements[];
+  facilitatorURL: string;
+};
+
+export async function createMiddleware(args: CreateMiddlewareArgs) {
+  const t = await fetch(`${args.facilitatorURL}/accepts`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      x402Version: 1,
+      accepts: args.accepts,
+    }),
+  });
+
+  const cachedPaymentRequirements = x402PaymentRequiredResponse(await t.json());
+
+  if (isValidationError(cachedPaymentRequirements)) {
+    throw new Error(
+      `invalid payment requirements from facilitator: ${cachedPaymentRequirements.summary}`,
+    );
+  }
+
+  const sendPaymentRequired = async (res: Response) => {
+    res.status(402).json(cachedPaymentRequirements);
+  };
+
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const paymentHeader = req.header("X-PAYMENT");
+    if (!paymentHeader) {
+      return sendPaymentRequired(res);
+    }
+
+    const payload = x402PaymentHeaderToPayload(paymentHeader);
+
+    if (isValidationError(payload)) {
+      return sendPaymentRequired(res);
+    }
+
+    // XXX - This is naive, and doesn't consider `asset` because that information
+    // isn't available here.
+    const paymentRequirements = cachedPaymentRequirements.accepts.filter(
+      (x) => x.network === payload.network && x.scheme === payload.scheme,
+    )[0];
+
+    if (!paymentRequirements) {
+      return sendPaymentRequired(res);
+    }
+
+    const settleRequest: x402SettleRequest = {
+      x402Version: 1,
+      paymentHeader,
+      paymentRequirements,
+    };
+
+    const t = await fetch(`${args.facilitatorURL}/settle`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(settleRequest),
+    });
+    const v = await t.json();
+    const settlementResponse = x402SettleResponse(v);
+
+    if (isValidationError(settlementResponse)) {
+      throw new Error(
+        `error getting response from facilitator for settlement: ${settlementResponse.summary}`,
+      );
+    }
+
+    if (!settlementResponse.success) {
+      return sendPaymentRequired(res);
+    }
+
+    next();
   };
 }

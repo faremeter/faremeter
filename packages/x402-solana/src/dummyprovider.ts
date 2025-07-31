6002c726839f3868711e32392cf4c3a18b78af33
Alexander Guy <alexander.guy@pm.me>
2025-07-30 17:43:19 -0700

    Remove @faremeter/x402-solana

--- packages/x402-solana/src/dummyprovider.ts
+++
@@ -1,8 +0,0 @@
-import type { Provider } from "@coral-xyz/anchor";
-import type { Connection } from "@solana/web3.js";
-
-export class DummyProvider implements Provider {
-  public get connection(): Connection {
-    throw new Error("anchor requested a connection");
-  }
-}

2d137979c6decf9806b0963a04df7435ff740a87
Alexander Guy <alexander.guy@pm.me>
2025-08-11 17:28:50 -0700

    Add support for Hono middleware

---
+++ packages/middleware/src/hono.ts
@@ -0,0 +1,103 @@
+import {
+  x402SettleRequest,
+  x402SettleResponse,
+  x402PaymentRequiredResponse,
+  x402PaymentRequirements,
+  x402PaymentHeaderToPayload,
+  isValidationError,
+} from "@faremeter/types";
+import { findMatchingPaymentRequirements } from "./utils";
+
+import type { MiddlewareHandler } from "hono";
+
+type CreateMiddlewareArgs = {
+  accepts: x402PaymentRequirements[];
+  facilitatorURL: string;
+};
+
+export async function createMiddleware(
+  args: CreateMiddlewareArgs,
+): Promise<MiddlewareHandler> {
+  async function getPaymentRequiredResponse(args: CreateMiddlewareArgs) {
+    const t = await fetch(`${args.facilitatorURL}/accepts`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        x402Version: 1,
+        accepts: args.accepts,
+      }),
+    });
+
+    const response = x402PaymentRequiredResponse(await t.json());
+
+    if (isValidationError(response)) {
+      throw new Error(
+        `invalid payment requirements from facilitator: ${response.summary}`,
+      );
+    }
+
+    return response;
+  }
+
+  return async (c, next) => {
+    // XXX - Temporarily request this every time.  This will be
+    // cached in future.
+    const paymentRequiredResponse = await getPaymentRequiredResponse(args);
+    const sendPaymentRequired = async () => {
+      c.status(402);
+      return c.json(paymentRequiredResponse);
+    };
+
+    const paymentHeader = c.req.header("X-PAYMENT");
+    if (!paymentHeader) {
+      return sendPaymentRequired();
+    }
+
+    const payload = x402PaymentHeaderToPayload(paymentHeader);
+
+    if (isValidationError(payload)) {
+      return sendPaymentRequired();
+    }
+
+    const paymentRequirements = findMatchingPaymentRequirements(
+      paymentRequiredResponse.accepts,
+      payload,
+    );
+
+    if (!paymentRequirements) {
+      return sendPaymentRequired();
+    }
+
+    const settleRequest: x402SettleRequest = {
+      x402Version: 1,
+      paymentHeader,
+      paymentRequirements,
+    };
+
+    const t = await fetch(`${args.facilitatorURL}/settle`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(settleRequest),
+    });
+    const v = await t.json();
+    const settlementResponse = x402SettleResponse(v);
+
+    if (isValidationError(settlementResponse)) {
+      throw new Error(
+        `error getting response from facilitator for settlement: ${settlementResponse.summary}`,
+      );
+    }
+
+    if (!settlementResponse.success) {
+      return sendPaymentRequired();
+    }
+
+    await next();
+  };
+}
--- packages/middleware/src/index.ts
+++ packages/middleware/src/index.ts
@@ -1,1 +1,2 @@
 export * as express from "./express";
+export * as hono from "./hono";

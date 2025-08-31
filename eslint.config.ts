179ae6577ea41958ec195afbdef495a4f04db15e
Alexander Guy <alexander.guy@pm.me>
2025-08-31 09:33:31 -0700

    Temporarily disable typed eslint checks
    This is causing problems with clean repositories and needs
    to get fixed in a real way. Until then, disable it.

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,25 +4,14 @@ import * as eslint from "@eslint/js";
 import * as tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
 
-const insideStagingDir = process.env.INSIDE_STAGING_DIR == "true";
-
 const config: tseslint.Config = tseslint.config(
   eslint.configs.recommended,
-  insideStagingDir
-    ? tseslint.configs.strict
-    : tseslint.configs.strictTypeChecked,
-  insideStagingDir
-    ? tseslint.configs.stylistic
-    : tseslint.configs.stylisticTypeChecked,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
   globalIgnores(["**/idl_type.ts", "**/dist/**"]),
   {
     rules: {
       "@typescript-eslint/consistent-type-definitions": 0,
-      "@typescript-eslint/restrict-template-expressions": 0,
-      "@typescript-eslint/no-confusing-void-expression": 0,
-      "@typescript-eslint/require-await": 0,
-      "@typescript-eslint/no-unnecessary-condition": 0,
-      "@typescript-eslint/no-unsafe-argument": 0,
     },
     languageOptions: {
       parserOptions: {

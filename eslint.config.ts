ec9e25b9742b06ab285ac0a45f814fa6a214b524
Alexander Guy <alexander.guy@pm.me>
2025-09-01 11:36:00 -0700

    Turn type-checked eslint back on

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,14 +4,25 @@ import * as eslint from "@eslint/js";
 import * as tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
 
+const insideStagingDir = process.env.INSIDE_STAGING_DIR == "true";
+
 const config: tseslint.Config = tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
+  insideStagingDir
+    ? tseslint.configs.strict
+    : tseslint.configs.strictTypeChecked,
+  insideStagingDir
+    ? tseslint.configs.stylistic
+    : tseslint.configs.stylisticTypeChecked,
   globalIgnores(["**/idl_type.ts", "**/dist/**"]),
   {
     rules: {
       "@typescript-eslint/consistent-type-definitions": 0,
+      "@typescript-eslint/restrict-template-expressions": 0,
+      "@typescript-eslint/no-confusing-void-expression": 0,
+      "@typescript-eslint/require-await": 0,
+      "@typescript-eslint/no-unnecessary-condition": 0,
+      "@typescript-eslint/no-unsafe-argument": 0,
     },
     languageOptions: {
       parserOptions: {

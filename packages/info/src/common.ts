0a82911a335beb9e0b69a3e457d1a8c168f4b009
Alexander Guy <alexander.guy@pm.me>
2025-09-11 09:16:08 -0700

    Add helpers to generate payment requirements for middleware users
    These are standalone in order to minimize the number of packages
    middleware users need to add/import in their environment.

--- packages/info/src/common.ts
+++ packages/info/src/common.ts
@@ -1,1 +1,13 @@
+import { type x402PaymentRequirements } from "@faremeter/types";
+
 export type UnitInput = string | number;
+
+export function addX402PaymentRequirementDefaults(
+  req: Partial<x402PaymentRequirements>,
+) {
+  // These come from coinbase/x402's defaults.
+  req.description ??= "";
+  req.mimeType ??= "application/json";
+
+  return req;
+}

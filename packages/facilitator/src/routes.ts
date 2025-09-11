97494b8291dafadc0854a598b9072f2d1842625b
Alexander Guy <alexander.guy@pm.me>
2025-09-11 08:47:24 -0700

    Send 400 to the middleware when we detect errors
    This will probably need to become more nuanced in future (e.g. if it's
    a problem with the transaction versus the request).

--- packages/facilitator/src/routes.ts
+++ packages/facilitator/src/routes.ts
@@ -14,6 +14,7 @@ export function createFacilitatorRoutes(args: CreateFacilitatorRoutesArgs) {
 
   function sendError(c: Context, msg: string) {
     logger.error(msg);
+    c.status(400);
     return c.json({
       isValid: false,
       invalidReason: msg,

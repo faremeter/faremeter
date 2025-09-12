f01e4016814c85677d705856473075943b6a605c
Alexander Guy <alexander.guy@pm.me>
2025-09-12 11:28:16 -0700

    Narrow x402 payment requirements based on the asset
    This is more specific than what is up in coinbase/x402.  This
    change allows for us to have more than one asset for a given
    scheme/network.  Otherwise, it's too ambiguous.

--- packages/middleware/src/utils.ts
+++ packages/middleware/src/utils.ts
@@ -9,11 +9,33 @@ export function findMatchingPaymentRequirements(
   accepts: x402PaymentRequirements[],
   payload: x402PaymentPayload,
 ) {
-  // XXX - This is naive, and doesn't consider `asset` because that information
-  // isn't available here.
-  return accepts.find(
-    (x) => x.network === payload.network && x.scheme === payload.scheme,
-  );
+  let possible;
+
+  if (payload.asset !== undefined) {
+    // Narrow based on the asset if available.
+    possible = accepts.filter(
+      (x) =>
+        x.network === payload.network &&
+        x.scheme === payload.scheme &&
+        x.asset === payload.asset,
+    );
+  } else {
+    // Otherwise fall back to the behavior in coinbase/x402.
+    possible = accepts.filter(
+      (x) => x.network === payload.network && x.scheme === payload.scheme,
+    );
+  }
+
+  if (possible.length > 1) {
+    logger.warning(
+      `found ${possible.length} ambiguous matching requirements for client payment: {*}`,
+      payload,
+    );
+  }
+
+  // XXX - If there are more than one, this really should be an error.
+  // For now, err on the side of potential compatibility.
+  return possible[0];
 }
 
 export function gateGetPaymentRequiredResponse(res: Response) {

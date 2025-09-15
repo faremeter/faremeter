6f12ce16e80174a57156f48b9ecf0c16f950b439
Alexander Guy <alexander.guy@pm.me>
2025-09-15 15:43:03 -0700

    Refactor middleware request handling to a common interface

--- packages/middleware/src/hono.ts
+++ packages/middleware/src/hono.ts
@@ -1,81 +1,24 @@
-import {
-  x402SettleRequest,
-  x402SettleResponse,
-  x402PaymentHeaderToPayload,
-  isValidationError,
-} from "@faremeter/types";
-import {
-  findMatchingPaymentRequirements,
-  type RelaxedRequirements,
-  getPaymentRequiredResponse,
-} from "./common";
-
+import { handleMiddlewareRequest, type CommonMiddlewareArgs } from "./common";
 import type { MiddlewareHandler } from "hono";
 
-type CreateMiddlewareArgs = {
-  accepts: RelaxedRequirements[];
-  facilitatorURL: string;
-};
+type CreateMiddlewareArgs = CommonMiddlewareArgs;
 
 export async function createMiddleware(
   args: CreateMiddlewareArgs,
 ): Promise<MiddlewareHandler> {
   return async (c, next) => {
-    // XXX - Temporarily request this every time.  This will be
-    // cached in future.
-    const paymentRequiredResponse = await getPaymentRequiredResponse({
+    const response = await handleMiddlewareRequest({
       ...args,
       resource: c.req.url,
-    });
-    const sendPaymentRequired = () => {
-      c.status(402);
-      return c.json(paymentRequiredResponse);
-    };
-
-    const paymentHeader = c.req.header("X-PAYMENT");
-    if (!paymentHeader) {
-      return sendPaymentRequired();
-    }
-
-    const payload = x402PaymentHeaderToPayload(paymentHeader);
-
-    if (isValidationError(payload)) {
-      return sendPaymentRequired();
-    }
-
-    const paymentRequirements = findMatchingPaymentRequirements(
-      paymentRequiredResponse.accepts,
-      payload,
-    );
-
-    if (!paymentRequirements) {
-      return sendPaymentRequired();
-    }
-
-    const settleRequest: x402SettleRequest = {
-      x402Version: 1,
-      paymentHeader,
-      paymentRequirements,
-    };
-
-    const t = await fetch(`${args.facilitatorURL}/settle`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+      getHeader: (key) => c.req.header(key),
+      sendJSONResponse: (status, body) => {
+        c.status(status);
+        return c.json(body);
       },
-      body: JSON.stringify(settleRequest),
     });
-    const settlementResponse = x402SettleResponse(await t.json());
 
-    if (isValidationError(settlementResponse)) {
-      throw new Error(
-        `error getting response from facilitator for settlement: ${settlementResponse.summary}`,
-      );
-    }
-
-    if (!settlementResponse.success) {
-      return sendPaymentRequired();
+    if (response) {
+      return response;
     }
 
     await next();

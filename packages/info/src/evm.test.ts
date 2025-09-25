76f2e79ee2906ae4e60330186c350bfd31e520a1
TheGreatAxios <sawyer@dirtroad.dev>
2025-09-24 22:05:38 -0700

    Add EVM Unit Test for SKALE Europa Testnet

--- packages/info/src/evm.test.ts
+++ packages/info/src/evm.test.ts
@@ -19,6 +19,24 @@ await t.test("basicAssetLookup", async (t) => {
     t.matchOnly(info.address, "0x036cbd53842c5426634e7929541ec2318f3dcf7e");
     t.matchOnly(info.name, "USDC");
     t.matchOnly(info.network, "base-sepolia");
+    t.equal(info.forwarder, undefined);
+    t.equal(info.forwarderName, undefined);
+    t.equal(info.forwarderVersion, undefined);
+    t.end();
+  });
+  await t.test((t) => {
+    const info = evm.lookupKnownAsset("skale-europa-testnet", "USDC");
+
+    if (!info) {
+      throw new Error("failed to lookup known EVM asset");
+    }
+
+    t.matchOnly(info.address, "0x9eAb55199f4481eCD7659540A17Af618766b07C4");
+    t.matchOnly(info.name, "USDC");
+    t.matchOnly(info.network, "skale-europa-testnet");
+    t.matchOnly(info.forwarder, "0x7779B0d1766e6305E5f8081E3C0CDF58FcA24330");
+    t.matchOnly(info.forwarderName, "USDC Forwarder");
+    t.matchOnly(info.forwarderVersion, "1");
     t.end();
   });
   await t.test((t) => {

f52e6d0455201d5093ea76eac702e36239d9de67
Alexander Guy <alexander.guy@pm.me>
2025-10-16 16:58:55 -0700

    Cache payment requirements responses by default

--- packages/middleware/src/express.ts
+++ packages/middleware/src/express.ts
@@ -1,13 +1,17 @@
 import {
   handleMiddlewareRequest,
   type CommonMiddlewareArgs,
-  getPaymentRequiredResponse,
+  createPaymentRequiredResponseCache,
 } from "./common";
 import type { NextFunction, Request, Response } from "express";
 
 type createMiddlewareArgs = CommonMiddlewareArgs;
 
 export async function createMiddleware(args: createMiddlewareArgs) {
+  const { getPaymentRequiredResponse } = createPaymentRequiredResponseCache(
+    args.cacheConfig,
+  );
+
   return async (req: Request, res: Response, next: NextFunction) => {
     const response = await handleMiddlewareRequest({
       ...args,

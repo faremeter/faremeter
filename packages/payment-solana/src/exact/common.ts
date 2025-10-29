be8d86033539ee09335dd91332057a2c3db85e1a
Alexander Guy <alexander.guy@pm.me>
2025-10-29 14:11:15 -0700

    Unify how Solana and EVM payment requirements matchers get created

--- packages/payment-solana/src/exact/common.ts
+++ packages/payment-solana/src/exact/common.ts
@@ -1,17 +1,10 @@
-import { type } from "arktype";
-import { caseInsensitiveLiteral } from "@faremeter/types";
+import { generateRequirementsMatcher } from "@faremeter/types/x402";
 import { lookupX402Network } from "@faremeter/info/solana";
 
 export const x402Scheme = "exact";
 
 export function generateMatcher(network: string, asset: string) {
-  const matchTuple = type({
-    scheme: caseInsensitiveLiteral(x402Scheme),
-    network: caseInsensitiveLiteral(...lookupX402Network(network)),
-    asset: caseInsensitiveLiteral(asset),
-  });
-
-  return {
-    matchTuple,
-  };
+  return generateRequirementsMatcher([x402Scheme], lookupX402Network(network), [
+    asset,
+  ]);
 }
